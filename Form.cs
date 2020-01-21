64e656efae727616bba0dffa83fc36f077a3b0c8
StraykerPL <strayker@op.pl>
2020-01-21 18:33:08 +0100

    Added comments to code, small changes in code

--- Form.cs
+++ Form.cs
@@ -13,7 +13,7 @@ namespace Reflex_Trainer
 {
     public partial class Form : System.Windows.Forms.Form
     {
-        public Trainer trainer;
+        public Trainer trainer; // Holds timer instance,
 
         public Form()
         {
@@ -21,8 +21,10 @@ namespace Reflex_Trainer
             StatusLabel.Text = "Welcome to Reflex Trainer!\nClick here to start training!";
         }
 
+        // Executes when "Save score" in menu is clicked:
         private void SaveScoreToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            // Checks, if app is in valid state to save score:
             if(trainer == null || StatusLabel.Text == "Too fast!" || trainer.GetBestTime().Milliseconds == 0)
             {
                 MessageBox.Show("There is no achieved score in this Reflex Trainer session or trying saving too-fast-clicked score!", "Reflex Trainer", MessageBoxButtons.OK);
@@ -33,8 +35,9 @@ namespace Reflex_Trainer
             folderBrowserDialog1.ShowNewFolderButton = true;
             folderBrowserDialog1.ShowDialog();
 
-            if (folderBrowserDialog1.SelectedPath == "") return;
+            if (folderBrowserDialog1.SelectedPath == "") return; // If user didn't choose the path,
 
+            // Trying to save score:
             try
             {
                 var save = new StreamWriter(folderBrowserDialog1.SelectedPath + "\\Score.txt");
@@ -52,37 +55,44 @@ namespace Reflex_Trainer
             MessageBox.Show("Score saved successful!", "Reflex Trainer", MessageBoxButtons.OK);
         }
 
+        // Executes when "Start training" in menu is clicked:
         private void StartTrainingElement_Click(object sender, EventArgs e)
         {
             StartTraining();
         }
 
+        // Executes when "Stop training" in menu is clicked:
         private void StopTrainingElement_Click(object sender, EventArgs e)
         {
             StopTraining();
             trainer.ClearScore();
         }
 
+        // Executes when "Exit" in menu is clicked:
         private void ExitElement_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
+        // Executes when "Documentation" in menu is clicked:
         private void DocsElement_Click(object sender, EventArgs e)
         {
             Process.Start("http://straykerpl.github.io");
         }
 
+        // Executes when "About app" in menu is clicked:
         private void AboutAppElement_Click(object sender, EventArgs e)
         {
             var info = new AboutAppWindow();
             info.ShowDialog();
         }
 
+        // Executes when training area is clicked:
         private void AreaClicked(object sender, EventArgs e)
         {
+            // Check, if user clicked in area when color is green:
             if(trainer.GetAreaColor() == Color.Green)
-            {
+            { // Green color:
                 trainer.StopCounting();
                 var time = trainer.GetBestTime();
                 StatusLabel.Text = string.Concat("Your reaction time: " + time.Milliseconds + " ms!\nClick here to start again!");
@@ -90,7 +100,7 @@ namespace Reflex_Trainer
                 MenuControl(true);
             }
             else
-            {
+            { // Red color:
                 trainer.StopCounting();
                 StatusLabel.Text = "Too fast!";
                 StatusPanel.Visible = true;
@@ -98,46 +108,51 @@ namespace Reflex_Trainer
             }
         }
 
+        // Method to block or unblock menu entries, true for no-training mode, false for training mode:
         public void MenuControl(bool value)
         {
             var a = menuStrip1.Items.Find("StartTrainingElement", true); // Start training,
             var b = menuStrip1.Items.Find("SaveScoreElement", true); // Save score,
             var c = menuStrip1.Items.Find("HelpElement", true); // Help menu,
-            var d = menuStrip1.Items.Find("StopTrainingElement", true);
+            var d = menuStrip1.Items.Find("StopTrainingElement", true); // Stop training,
 
             a[0].Enabled = value;
             b[0].Enabled = value;
             c[0].Enabled = value;
-            d[0].Enabled = !value;
+            d[0].Enabled = !value; // Negation here, because "Stop training" menu must be blocked in non-training mode,
         }
 
+        // Starts training by clicking greeting panel:
         private void StatusPanel_Click(object sender, EventArgs e)
         {
             StartTraining();
         }
 
+        // Starts training by clicking greeting message:
         private void StatusLabel_Click(object sender, EventArgs e)
         {
             StartTraining();
         }
 
+        // Method for starting training instructions:
         private void StartTraining()
         {
-            MenuControl(false);
-            StatusPanel.Visible = false;
-            trainer = new Trainer(AreaBox);
-            trainer.RedArea();
-            AreaBox.Click += new EventHandler(AreaClicked);
-            trainer.StartCounting();
+            MenuControl(false); // Blocking menu entries,
+            StatusPanel.Visible = false; // Hide welcome panel,
+            trainer = new Trainer(AreaBox); // Create instance of trainer,
+            trainer.RedArea(); // Chceck, if training area is red,
+            AreaBox.Click += new EventHandler(AreaClicked); // Make sure click handler is added,
+            trainer.StartCounting(); // Start counting of time,
         }
 
+        // Method for stopping training instructions:
         private void StopTraining()
         {
-            MenuControl(true);
-            StatusPanel.Visible = true;
-            StatusLabel.Text = "Welcome to Reflex Trainer!\nClick here to start training!";
-            trainer.StopCounting();
-            trainer.RedArea();
+            MenuControl(true); // Unblock the menu,
+            StatusPanel.Visible = true; // Show welcome panel...
+            StatusLabel.Text = "Welcome to Reflex Trainer!\nClick here to start training!"; // ... and display the welcome message,
+            trainer.StopCounting(); // Stop all counting,
+            trainer.RedArea(); // Set training area to red again,
         }
     }
 }
--- Trainer.cs
+++ Trainer.cs
@@ -11,32 +11,37 @@ using System.Windows.Forms;
 
 namespace Reflex_Trainer
 {
+    // Class for managing training in app:
     public class Trainer
     {
-        private TimeSpan BestTime;
-        private Color AreaColor;
-        private Stopwatch Timekeeper;
-        private System.Timers.Timer ColorChangeTimekeeper;
-        private readonly Random RandGen = new Random();
-        private readonly PictureBox Area;
+        private TimeSpan BestTime; // Holds best time,
+        private Color AreaColor; // Holds color of training area,
+        private Stopwatch Timekeeper; // Counts time for training session,
+        private System.Timers.Timer ColorChangeTimekeeper; // Counts time for changing background from red to green,
+        private readonly Random RandGen = new Random(); // Random generator,
+        private readonly PictureBox Area; // Holds the trainig area for class's methods,
 
+        // Constructor, this class needs training area to operate:
         public Trainer(PictureBox Area)
         {
             this.Area = Area;
         }
 
+        // Sets training area to red color:
         public void RedArea()
         {
             AreaColor = Color.Red;
             Area.BackColor = AreaColor;
         }
 
+        // Sets training area to green color:
         public void GreenArea()
         {
             AreaColor = Color.Green;
             Area.BackColor = AreaColor;
         }
 
+        // Start counter for reflex time:
         public void StartCounting()
         {
             Timekeeper = new Stopwatch();
@@ -47,28 +52,33 @@ namespace Reflex_Trainer
             ColorChangeTimekeeper.Enabled = true;
         }
 
+        // Stops all counters:
         public void StopCounting()
         {
             Timekeeper.Stop();
             ColorChangeTimekeeper.Enabled = false;
         }
 
+        // Gets the best time for last session:
         public TimeSpan GetBestTime()
         {
             BestTime = Timekeeper.Elapsed;
             return BestTime;
         }
 
+        // Gets active area color:
         public Color GetAreaColor()
         {
             return AreaColor;
         }
 
+        // This method rises when it's time to change color from red to green:
         private void TimerTick(object a, ElapsedEventArgs b)
         {
             GreenArea();
         }
 
+        // Clears best score from app memory:
         public void ClearScore()
         {
             BestTime = new TimeSpan(0, 0, 0, 0, 0);
